cfa5416f6f1ac0193c7eacb086fb634a6aebe18c
ibktommy <atomquartz@gmail.com>
2022-09-19 21:34:22 +0100

    Remove space

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import SearchForm from './components/SearchForm';
 import Buttons from './components/Buttons';
 import Stories from './components/Stories';
 
-
-
 function App() {
   return (
     <>
