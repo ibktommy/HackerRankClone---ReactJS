ef9f5198833de5639337b36bd4766f75ffc5602c
ibktommy <atomquartz@gmail.com>
2022-09-19 12:05:16 +0100

    Setup the SET_lOADING-action in the reducer file and get it in the AppContext as a global-tate and then use this use the global-state to conditionally render elements in the Stories component'

--- src/reducer.js
+++ src/reducer.js
@@ -6,5 +6,11 @@ import {
   HANDLE_SEARCH,
 } from './actions'
 
-const reducer = () => { }
+const reducer = (state, action) => {
+
+  switch (action.type) {
+    case 'SET_LOADING': return { ...state, isLoading: true }
+    default: throw new Error(`No Matching Type`)
+  }
+}
 export default reducer
