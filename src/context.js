7dd668e006651f8ad55d81d38ee1152b0ef255fe
ibktommy <atomquartz@gmail.com>
2022-09-19 11:28:51 +0100

    Setting up Reducer

--- src/context.js
+++ src/context.js
@@ -1,9 +1,25 @@
-import React, { useContext } from 'react'
+import React, { useContext, useReducer } from 'react'
+import {
+  SET_LOADING,
+  SET_STORIES,
+  REMOVE_STORY,
+  HANDLE_PAGE,
+  HANDLE_SEARCH,
+} from './actions'
+import reducer from './reducer'
+
+const mainUrl = 'https://hn.algolia.com/api/v1/search?'
+
+// Setting up Reducer
+const initialState = {}
 
 // Implementing Context for the APP
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+
   return <AppContext.Provider value={'HEllo'}>
     {children}
   </AppContext.Provider>
