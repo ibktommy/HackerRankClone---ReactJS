30aca3c278e9b722b90bcf0e1499f2724805da4f
ibktommy <atomquartz@gmail.com>
2022-09-19 16:54:44 +0100

    Add elements and their styles to the fetched data-object-values from the external server

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -12,11 +12,20 @@ const Stories = () => {
   return (
     <div className="stories">
       {
-        responseData.map((item, index) => {
-          console.log(item)
+        responseData.map((item) => {
+          // Destructuring item Object Values
+          const { objectID: id, title, num_comments, url, points, author } = item
           return (
-            <article key={index} className='story'>
-              Single STory
+            <article key={id} className='story'>
+              <h4 className='title'>{title}</h4>
+              <p className="info">{points} points by <span>{author}</span> | <span>{num_comments}{' '} comments</span>
+              </p>
+              <div>
+                <a href={url} className='read-link' target='_blank' rel='noopener noreferrer'>
+                  read more
+                </a>
+                <button className='remove-btn'>remove</button>
+              </div>
             </article>
           )
         })
